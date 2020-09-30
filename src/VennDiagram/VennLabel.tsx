5ff313eb49aa52ae653236922abe4db4dde89b9e
Austin <amcdaniel2@gmail.com>
2020-09-30 11:18:13 -0400

    label size calc

--- src/VennDiagram/VennLabel.tsx
+++ src/VennDiagram/VennLabel.tsx
@@ -1,15 +1,27 @@
 import React, { FC } from 'react';
 import { IVennLayout } from '@upsetjs/venn.js';
+import { calculateDimensions } from '../common/utils';
 
 export interface VennLabelProps {
   data: IVennLayout<any>;
+  fontSize: number;
+  fontFamily: string;
 }
 
-export const VennLabel: FC<VennLabelProps> = ({ data }) => {
+export const VennLabel: FC<VennLabelProps> = ({
+  data,
+  fontSize = 11,
+  fontFamily = 'sans-serif',
+}) => {
   const key = data.data.sets.join(' | ');
+  const size = calculateDimensions(key, fontFamily, fontSize);
+  const halfHeight = size.height / 2;
+  const halfWidth = size.width / 2;
+  const x = data.text.x - halfWidth;
+  const y = data.text.y + halfHeight;
 
   return (
-    <text x={data.text.x} y={data.text.y}>
+    <text x={x} y={y}>
       {key}
     </text>
   );
