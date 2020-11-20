f5aedf85fc492a16dd314fe3bdf5c5e3eae0ee8d
Friedrich Maeckle <maeckle@nantis.de>
2020-11-20 14:09:53 +0100

    Add missing exports

--- src/RadialGauge/RadialGaugeSeries/index.ts
+++ src/RadialGauge/RadialGaugeSeries/index.ts
@@ -2,3 +2,5 @@ export * from './RadialGaugeSeries';
 export * from './RadialGaugeArc';
 export * from './RadialGaugeLabel';
 export * from './RadialGaugeValueLabel';
+export * from './StackedRadialGaugeSeries';
+export * from './StackedRadialGaugeLabel';
