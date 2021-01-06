6e94b7d5f69b841956b3be642900bfad23d74015
yohainjug <yohainjug@gmail.com>
2021-01-06 16:48:28 +0200

    debugging  hiveplot

--- src/HivePlot/HivePlot.tsx
+++ src/HivePlot/HivePlot.tsx
@@ -1,5 +1,4 @@
-import React, { Fragment, Component } from 'react';
-import bind from 'memoize-bind';
+import React, { Fragment, useCallback, FC, useState } from 'react';
 import { scaleOrdinal, scaleLinear, scalePoint } from 'd3-scale';
 import { range } from 'd3-array';
 import { Tooltip } from 'realayers';
@@ -59,64 +58,59 @@ export interface HivePlotProps {
   };
 }
 
-interface HivePlotState {
-  tooltipReference?: EventTarget | null;
-  nodeTooltipData?: Node;
-  linkTooltipData?: Link;
-  active?: { [k: string]: boolean };
-  height?: number;
-  width?: number;
-}
+export const HivePlot: FC<Partial<HivePlotProps>> = ({
+  axis = [],
+  nodes = [],
+  links = [],
+  disabled = false,
+  activeIds = [],
+  label = {
+    show: true,
+    padding: 10
+  },
+  width,
+  height,
+  innerRadius = 20,
+  className,
+  onNodeClick = () => undefined,
+  onNodeMouseOver = () => undefined,
+  onLinkMouseOver = () => undefined,
+  onNodeMouseOut = () => undefined,
+  onLinkMouseOut = () => undefined,
+  tooltip = {
+    show: true,
+    placement: 'top',
+    formatter: (attr: any) => attr.value
+  },
+  colorScheme = {
+    axis: ['#b1b2b6'],
+    domain: ['#b1b2b6']
+  }
+}) => {
+  const [tooltipReference, setTooltipReference] = useState<EventTarget | null>(
+    null
+  );
+  const [nodeTooltipData, setNodeTooltipData] = useState<Node | null>(null);
+  const [linkTooltipData, setLinkTooltipData] = useState<Link | null>(null);
+  const [active, setActive] = useState<{ [k: string]: boolean } | null>(null);
+  console.log('initial, ', width, height);
+  const onNodeMouseOverLocal = useCallback(
+    (node: Node, event: MouseEvent) => {
+      if (!disabled) {
+        const activeNodeIndex = nodes.indexOf(node);
+        const activeNodes = {};
 
-export class HivePlot extends Component<HivePlotProps, HivePlotState> {
-  static defaultProps = {
-    innerRadius: 20,
-    disabled: false,
-    axis: [],
-    nodes: [],
-    links: [],
-    label: {
-      show: true,
-      padding: 10
-    },
-    activeIds: [],
-    onNodeClick: () => undefined,
-    onNodeMouseOver: () => undefined,
-    onNodeMouseOut: () => undefined,
-    onLinkMouseOver: () => undefined,
-    onLinkMouseOut: () => undefined,
-    tooltip: {
-      show: true,
-      placement: 'top',
-      formatter: (attr) => attr.value
-    },
-    colorScheme: {
-      axis: ['#b1b2b6'],
-      domain: ['#b1b2b6']
-    }
-  };
-
-  state: HivePlotState = {};
-
-  onNodeMouseOver(node: Node, event: MouseEvent) {
-    const { links, nodes, onNodeMouseOver, disabled } = this.props;
-    if (!disabled) {
-      const activeNodeIndex = nodes.indexOf(node);
-      const activeNodes = {};
-
-      for (const link of links) {
-        const { source, target } = link;
-        if (source.value === node.value || target.value === node.value) {
-          const next = target.value === node.value ? source : target;
-          const idx = nodes.indexOf(next);
-          activeNodes[`node-${idx}`] = true;
+        for (const link of links) {
+          const { source, target } = link;
+          if (source.value === node.value || target.value === node.value) {
+            const next = target.value === node.value ? source : target;
+            const idx = nodes.indexOf(next);
+            activeNodes[`node-${idx}`] = true;
+          }
         }
-      }
-
-      this.setState({
-        tooltipReference: event.target,
-        nodeTooltipData: node,
-        active: {
+        setTooltipReference(event.target);
+        setNodeTooltipData(node);
+        setActive({
           [`node-${activeNodeIndex}`]: true,
           ...activeNodes,
           ...links.reduce((accumulator, link, i) => {
@@ -128,46 +122,49 @@ export class HivePlot extends Component<HivePlotProps, HivePlotState> {
             }
             return accumulator;
           }, {})
+        });
+      }
+
+      onNodeMouseOver({
+        nativeEvent: event,
+        node,
+        links: getLinksForNode(node)
+      });
+    },
+    [links, nodes, onNodeMouseOver, disabled]
+  );
+
+  const activateAdjacentLinks = useCallback(
+    (links: Link[], target: Node, accumulator: {}) => {
+      const activeLinks: any[] = [];
+
+      links.forEach((childLink, index) => {
+        if (target === childLink.source) {
+          if (!accumulator[`link-${index}`]) {
+            accumulator[`link-${index}`] = true;
+            activeLinks.push(
+              childLink,
+              ...activateAdjacentLinks(links, childLink.target, accumulator)
+            );
+          }
         }
       });
-    }
+      return activeLinks;
+    },
+    []
+  );
+  const activateLink = useCallback(
+    (link: Link) => {
+      const activeLinkIndex = links.indexOf(link);
+      const activeLinksMap = {
+        [`link-${activeLinkIndex}`]: true
+      };
 
-    onNodeMouseOver({
-      nativeEvent: event,
-      node,
-      links: this.getLinksForNode(node)
-    });
-  }
-
-  onLinkMouseOver(link: Link, event: MouseEvent) {
-    const { onLinkMouseOver, disabled } = this.props;
-    if (!disabled) {
-      this.setState({
-        tooltipReference: event.target,
-        linkTooltipData: link
-      });
-      this.activateLink(link);
-    }
-
-    onLinkMouseOver({
-      nativeEvent: event,
-      link
-    });
-  }
-
-  activateLink(link: Link) {
-    const { nodes, links } = this.props;
-    const activeLinkIndex = links.indexOf(link);
-    const activeLinksMap = {
-      [`link-${activeLinkIndex}`]: true
-    };
-
-    const activeLinks = [
-      link,
-      ...this.activateAdjacentLinks(links, link.target, activeLinksMap)
-    ];
-    this.setState({
-      active: {
+      const activeLinks = [
+        link,
+        ...activateAdjacentLinks(links, link.target, activeLinksMap)
+      ];
+      setActive({
         ...activeLinksMap,
         ...nodes.reduce((accumulator, node, i) => {
           for (const activeLink of activeLinks) {
@@ -178,197 +175,208 @@ export class HivePlot extends Component<HivePlotProps, HivePlotState> {
           }
           return accumulator;
         }, {})
+      });
+    },
+    [nodes, links]
+  );
+
+  const onLinkMouseOverLocal = useCallback(
+    (link: Link, event: MouseEvent) => {
+      if (!disabled) {
+        setTooltipReference(event.target);
+        setLinkTooltipData(link);
+        activateLink(link);
       }
-    });
-  }
 
-  activateAdjacentLinks(links: Link[], target: Node, accumulator: {}) {
-    const activeLinks: any[] = [];
+      onLinkMouseOver({
+        nativeEvent: event,
+        link
+      });
+    },
+    [onLinkMouseOver, disabled]
+  );
 
-    links.forEach((childLink, index) => {
-      if (target === childLink.source) {
-        if (!accumulator[`link-${index}`]) {
-          accumulator[`link-${index}`] = true;
-          activeLinks.push(
-            childLink,
-            ...this.activateAdjacentLinks(links, childLink.target, accumulator)
-          );
-        }
-      }
-    });
+  const getLinksForNode = useCallback(
+    (node: Node): Link[] =>
+      links.filter(
+        (link) =>
+          link.source.value === node.value || link.target.value === node.value
+      ),
+    [links]
+  );
+  const resetActive = () => {
+    setActive(null);
+    setLinkTooltipData(null);
+    setNodeTooltipData(null);
+    setTooltipReference(null);
+  };
 
-    return activeLinks;
-  }
-
-  onNodeMouseOut(node: Node, event: MouseEvent) {
-    const { onNodeMouseOut } = this.props;
-    this.resetActive();
-    onNodeMouseOut({
-      nativeEvent: event,
-      node,
-      links: this.getLinksForNode(node)
-    });
-  }
-
-  onLinkMouseOut(link: Link, event: MouseEvent) {
-    const { onLinkMouseOut } = this.props;
-    this.resetActive();
-    onLinkMouseOut({
-      nativeEvent: event,
-      link
-    });
-  }
-
-  onNodeClick(node: Node, event: MouseEvent) {
-    const { disabled } = this.props;
-    if (!disabled) {
-      const { onNodeClick } = this.props;
-      onNodeClick({
+  const onNodeMouseOutLocal = useCallback(
+    (node: Node, event: MouseEvent) => {
+      resetActive();
+      onNodeMouseOut({
         nativeEvent: event,
         node,
-        links: this.getLinksForNode(node)
+        links: getLinksForNode(node)
       });
-    }
-  }
+    },
+    [onNodeMouseOut]
+  );
 
-  getLinksForNode(node: Node): Link[] {
-    const { links } = this.props;
-    return links.filter(
-      (link) =>
-        link.source.value === node.value || link.target.value === node.value
-    );
-  }
+  const onLinkMouseOutLocal = useCallback(
+    (link: Link, event: MouseEvent) => {
+      resetActive();
+      onLinkMouseOut({
+        nativeEvent: event,
+        link
+      });
+    },
+    [onLinkMouseOut]
+  );
 
-  resetActive() {
-    this.setState({
-      active: undefined,
-      linkTooltipData: undefined,
-      nodeTooltipData: undefined,
-      tooltipReference: undefined
-    });
-  }
+  const onNodeClickLocal = useCallback(
+    (node: Node, event: MouseEvent) => {
+      if (!disabled) {
+        onNodeClick({
+          nativeEvent: event,
+          node,
+          links: getLinksForNode(node)
+        });
+      }
+    },
+    [disabled, onNodeClick]
+  );
 
-  prepareData({ dimension, innerRadius, colorScheme, axis, label }) {
-    let outerRadius = dimension / 2;
-    if (label.show) {
-      outerRadius = outerRadius - (10 + label.padding);
-    }
+  const prepareData = useCallback(
+    ({ dimension, innerRadius, colorScheme, axis, label }) => {
+      let outerRadius = dimension / 2;
+      if (label.show) {
+        outerRadius = outerRadius - (10 + label.padding);
+      }
 
-    return {
-      angle: scalePoint()
-        .domain(range(axis.length + 1) as any)
-        .range([0, 2 * Math.PI]),
-      radius: scaleLinear().range([innerRadius, outerRadius]),
-      axisColor: scaleOrdinal(colorScheme.axis).domain(range(20) as any),
-      domainColor: scaleOrdinal(colorScheme.domain).domain(range(20) as any),
-      outerRadius
-    };
-  }
+      return {
+        angle: scalePoint()
+          .domain(range(axis.length + 1) as any)
+          .range([0, 2 * Math.PI]),
+        radius: scaleLinear().range([innerRadius, outerRadius]),
+        axisColor: scaleOrdinal(colorScheme.axis).domain(range(20) as any),
+        domainColor: scaleOrdinal(colorScheme.domain).domain(range(20) as any),
+        outerRadius
+      };
+    },
+    []
+  );
 
-  renderAxis({ angle, radius, axisColor, outerRadius }) {
-    const { axis, label } = this.props;
-
-    return (
-      <Fragment>
-        {axis.map((a, i) => (
-          <g key={`axis-${a.attribute}`}>
-            <HiveAxis
-              angle={angle}
-              index={i}
-              color={axisColor}
-              radius={radius}
-            />
-            {label.show && (
-              <HiveLabel
-                index={i}
-                text={a.label}
-                label={label}
-                outerRadius={outerRadius}
+  const renderAxis = useCallback(
+    ({ angle, radius, axisColor, outerRadius }) => {
+      return (
+        <Fragment>
+          {axis.map((a, i) => (
+            <g key={`axis-${a.attribute}`}>
+              <HiveAxis
                 angle={angle}
+                index={i}
+                color={axisColor}
+                radius={radius}
               />
-            )}
-          </g>
-        ))}
-      </Fragment>
-    );
-  }
+              {label.show && (
+                <HiveLabel
+                  index={i}
+                  text={a.label}
+                  label={label}
+                  outerRadius={outerRadius}
+                  angle={angle}
+                />
+              )}
+            </g>
+          ))}
+        </Fragment>
+      );
+    },
+    [axis, label]
+  );
 
-  isActive(nodeOrLink: Node | Link, index: number, type: 'node' | 'link') {
-    const { activeIds } = this.props;
-    const { active } = this.state;
-    // If no there is nothing active, then everything is active.
-    if (!active && !activeIds!.length) {
-      return true;
-    }
+  const isActive = useCallback(
+    (nodeOrLink: Node | Link, index: number, type: 'node' | 'link') => {
+      // If no there is nothing active, then everything is active.
+      if (!active && !activeIds!.length) {
+        return true;
+      }
 
-    // If this node is active because it is being hovered
-    if (active && active[`${type}-${index}`]) {
-      return true;
-    }
-    // If the ID matches one of the active IDs passed in the props
-    if (
-      !!activeIds!.length &&
-      !!nodeOrLink.id &&
-      activeIds!.includes(nodeOrLink.id)
-    ) {
-      return true;
-    }
-    return false;
-  }
+      // If this node is active because it is being hovered
+      if (active && active[`${type}-${index}`]) {
+        return true;
+      }
+      // If the ID matches one of the active IDs passed in the props
+      if (
+        !!activeIds!.length &&
+        !!nodeOrLink.id &&
+        activeIds!.includes(nodeOrLink.id)
+      ) {
+        return true;
+      }
+      return false;
+    },
+    [activeIds, active]
+  );
 
-  renderLinks({ angle, radius, domainColor }) {
-    const { links } = this.props;
-    return (
-      <Fragment>
-        {links.map((link, i) => {
-          return (
-            <HiveLink
-              key={`${link.value}-${i}`}
-              color={link.color || domainColor}
-              active={this.isActive(link, i, 'link')}
-              angle={angle}
+  const renderLinks = useCallback(
+    ({ angle, radius, domainColor }) => {
+      return (
+        <Fragment>
+          {links.map((link, i) => {
+            return (
+              <HiveLink
+                key={`${link.value}-${i}`}
+                color={link.color || domainColor}
+                active={isActive(link, i, 'link')}
+                angle={angle}
+                radius={radius}
+                link={link}
+                onMouseOver={(event: MouseEvent) =>
+                  onLinkMouseOverLocal(link, event)
+                }
+                onMouseOut={(event: MouseEvent) =>
+                  onLinkMouseOutLocal(link, event)
+                }
+              />
+            );
+          })}
+        </Fragment>
+      );
+    },
+    [links]
+  );
+
+  const renderNodes = useCallback(
+    ({ angle, radius, domainColor }) => {
+      return (
+        <Fragment>
+          {nodes.map((node, i) => (
+            <HiveNode
+              node={node}
+              key={`${node.value}-${i}`}
+              active={isActive(node, i, 'node')}
+              color={domainColor}
               radius={radius}
-              link={link}
-              onMouseOver={bind(this.onLinkMouseOver, this, link)}
-              onMouseOut={bind(this.onLinkMouseOut, this, link)}
+              angle={angle}
+              disabled={disabled}
+              onMouseOver={(event: MouseEvent) =>
+                onNodeMouseOverLocal(node, event)
+              }
+              onMouseOut={(event: MouseEvent) =>
+                onNodeMouseOutLocal(node, event)
+              }
+              onClick={(event: MouseEvent) => onNodeClickLocal(node, event)}
             />
-          );
-        })}
-      </Fragment>
-    );
-  }
+          ))}
+        </Fragment>
+      );
+    },
+    [nodes, disabled]
+  );
 
-  renderNodes({ angle, radius, domainColor }) {
-    const { nodes, disabled } = this.props;
-    return (
-      <Fragment>
-        {nodes.map((node, i) => (
-          <HiveNode
-            node={node}
-            key={`${node.value}-${i}`}
-            active={this.isActive(node, i, 'node')}
-            color={domainColor}
-            radius={radius}
-            angle={angle}
-            disabled={disabled}
-            onMouseOver={bind(this.onNodeMouseOver, this, node)}
-            onMouseOut={bind(this.onNodeMouseOut, this, node)}
-            onClick={bind(this.onNodeClick, this, node)}
-          />
-        ))}
-      </Fragment>
-    );
-  }
-
-  renderTooltip() {
-    const { tooltip, disabled, axis, nodes } = this.props;
-    const {
-      active,
-      tooltipReference,
-      linkTooltipData: link,
-      nodeTooltipData: node
-    } = this.state;
-
+  const renderTooltip = useCallback(() => {
     const { formatter, placement, show } = tooltip;
     return (
       <Fragment>
@@ -378,47 +386,51 @@ export class HivePlot extends Component<HivePlotProps, HivePlotState> {
             reference={tooltipReference}
             placement={placement}
             content={() =>
-              formatter(axis, nodes, link, node) ||
-              (node ? (
-                <HiveTooltip node={node} nodes={nodes} axis={axis} />
+              formatter(axis, nodes, linkTooltipData, nodeTooltipData) ||
+              (nodeTooltipData ? (
+                <HiveTooltip node={nodeTooltipData} nodes={nodes} axis={axis} />
               ) : null)
             }
           />
         )}
       </Fragment>
     );
-  }
+  }, [
+    tooltip,
+    disabled,
+    axis,
+    nodes,
+    active,
+    tooltipReference,
+    linkTooltipData,
+    nodeTooltipData
+  ]);
 
-  renderChart({ height, width }) {
-    const { innerRadius, axis, colorScheme, label, className } = this.props;
-
-    const data = this.prepareData({
+  const renderChart = useCallback(() => {
+    const data = prepareData({
       dimension: Math.min(height, width),
       innerRadius,
       colorScheme,
       axis,
       label
     });
-
+    console.log('height, width', height, width);
     return (
       <Fragment>
         <svg width={width} height={height} className={classNames(className)}>
           <g transform={`translate(${width / 2}, ${height / 2 + innerRadius})`}>
-            {this.renderAxis(data)}
-            {this.renderLinks(data)}
-            {this.renderNodes(data)}
+            {renderAxis(data)}
+            {renderLinks(data)}
+            {renderNodes(data)}
           </g>
         </svg>
-        {this.renderTooltip()}
+        {renderTooltip()}
       </Fragment>
     );
-  }
-
-  render() {
-    return (
-      <ChartContainer height={this.props.height} width={this.props.width}>
-        {this.renderChart.bind(this)}
-      </ChartContainer>
-    );
-  }
-}
+  }, [innerRadius, axis, colorScheme, label, className, height, width]);
+  return (
+    <ChartContainer height={height} width={width}>
+      {renderChart}
+    </ChartContainer>
+  );
+};
