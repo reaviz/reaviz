711276c7a0cf69fc8ba52b9728705d513b54a5f0
Austin <amcdaniel2@gmail.com>
2022-01-11 11:38:31 -0500

    remove unused prop

--- src/TreeMap/TreeMap.tsx
+++ src/TreeMap/TreeMap.tsx
@@ -1,7 +1,11 @@
 import React, { FC, ReactElement, useCallback } from 'react';
-import { ChartContainer, ChartContainerChildProps, ChartProps } from '../common/containers/ChartContainer';
+import {
+  ChartContainer,
+  ChartContainerChildProps,
+  ChartProps
+} from '../common/containers/ChartContainer';
 import { ChartShallowDataShape } from '../common/data';
-import { stratify, hierarchy, treemap, treemapSquarify } from 'd3-hierarchy';
+import { hierarchy, treemap, treemapSquarify } from 'd3-hierarchy';
 import { TreeMapSeries, TreeMapSeriesProps } from './TreeMapSeries';
 import { CloneElement } from 'rdk';
 
@@ -14,7 +18,7 @@ export interface TreeMapProps extends ChartProps {
   /**
    * The series component that renders the components.
    */
-   series?: ReactElement<TreeMapSeriesProps, typeof TreeMapSeries>;
+  series?: ReactElement<TreeMapSeriesProps, typeof TreeMapSeries>;
 }
 
 export const TreeMap: FC<Partial<TreeMapProps>> = ({
@@ -26,31 +30,37 @@ export const TreeMap: FC<Partial<TreeMapProps>> = ({
   margins,
   series
 }) => {
-  const getData = useCallback((cw: number, ch: number) => {
-    const root = hierarchy<any>({ children: data })
-      .sum(d => d.data)
-      .sort((a, b) => b.data - a.data);
+  const getData = useCallback(
+    (cw: number, ch: number) => {
+      const root = hierarchy<any>({ children: data })
+        .sum((d) => d.data)
+        .sort((a, b) => b.data - a.data);
 
-    const t = treemap()
-      .size([cw, ch])
-      .tile(treemapSquarify)
-      .round(true)
-      .padding(1);
+      const t = treemap()
+        .size([cw, ch])
+        .tile(treemapSquarify)
+        .round(true)
+        .padding(1);
 
-    return t(root).leaves();
-  }, [data]);
+      return t(root).leaves();
+    },
+    [data]
+  );
 
-  const renderChart = useCallback(({ chartWidth, chartHeight, ...rest }: ChartContainerChildProps) => {
-    const datas = getData(chartWidth, chartHeight);
-    return (
-      <CloneElement<TreeMapSeriesProps>
-        element={series}
-        {...rest}
-        id={`${rest.id}-series`}
-        data={datas}
-      />
-    );
-  }, [series, getData, id]);
+  const renderChart = useCallback(
+    ({ chartWidth, chartHeight, ...rest }: ChartContainerChildProps) => {
+      const datas = getData(chartWidth, chartHeight);
+      return (
+        <CloneElement<TreeMapSeriesProps>
+          element={series}
+          {...rest}
+          id={`${id}-series`}
+          data={datas}
+        />
+      );
+    },
+    [series, getData, id]
+  );
 
   return (
     <ChartContainer
