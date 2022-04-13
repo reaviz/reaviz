f1dd898cecf9dcb2a336d230efe88a562ce56ff4
Austin <amcdaniel2@gmail.com>
2022-04-13 08:55:50 -0400

    add story for 0 value

--- src/AreaChart/AreaChartSingleSeries.story.tsx
+++ src/AreaChart/AreaChartSingleSeries.story.tsx
@@ -16,10 +16,7 @@ import {
   StackedAreaSeries,
   StackedNormalizedAreaSeries
 } from './AreaSeries';
-import {
-  LinearXAxis,
-  LinearXAxisTickSeries
-} from '../common/Axis/LinearAxis';
+import { LinearXAxis, LinearXAxisTickSeries } from '../common/Axis/LinearAxis';
 import { Gradient, GradientStop } from '../common/Gradient';
 import { Stripes } from '../common/Mask';
 import { ChartDataShape } from '../common/data';
@@ -39,120 +36,124 @@ export default {
 };
 
 export const Simple = () => {
-    const height = number('Height', 250);
-    const width = number('Width', 350);
-    const lineStroke = number('Stroke Width', 4);
-    const color = select('Color Scheme', schemes, 'cybertron');
-    const interpolation = select(
-      'Interpolation',
-      {
-        linear: 'linear',
-        step: 'step',
-        smooth: 'smooth'
-      },
-      'linear'
-    );
-    const data = object('Data', singleDateData);
+  const height = number('Height', 250);
+  const width = number('Width', 350);
+  const lineStroke = number('Stroke Width', 4);
+  const color = select('Color Scheme', schemes, 'cybertron');
+  const interpolation = select(
+    'Interpolation',
+    {
+      linear: 'linear',
+      step: 'step',
+      smooth: 'smooth'
+    },
+    'linear'
+  );
+  const data = object('Data', singleDateData);
 
-    return (
-      <AreaChart
-        width={width}
-        height={height}
-        data={data}
-        series={
-          <AreaSeries
-            interpolation={interpolation}
-            colorScheme={color}
-            line={<Line strokeWidth={lineStroke} />}
-          />
-        }
-      />
-    );
-  };
-
-export const Masks = () => (
+  return (
     <AreaChart
-      width={350}
-      height={250}
-      data={singleDateData}
+      width={width}
+      height={height}
+      data={data}
       series={
         <AreaSeries
-          area={
-            <Area
-              mask={<Stripes />}
-              gradient={
-                <Gradient
-                  stops={[
-                    <GradientStop offset="10%" stopOpacity={0} />,
-                    <GradientStop offset="80%" stopOpacity={1} />
-                  ]}
-                />
-              }
-            />
-          }
-          line={<Line strokeWidth={3} />}
+          interpolation={interpolation}
+          colorScheme={color}
+          line={<Line strokeWidth={lineStroke} />}
         />
       }
     />
   );
+};
+
+export const Masks = () => (
+  <AreaChart
+    width={350}
+    height={250}
+    data={singleDateData}
+    series={
+      <AreaSeries
+        area={
+          <Area
+            mask={<Stripes />}
+            gradient={
+              <Gradient
+                stops={[
+                  <GradientStop offset="10%" stopOpacity={0} />,
+                  <GradientStop offset="80%" stopOpacity={1} />
+                ]}
+              />
+            }
+          />
+        }
+        line={<Line strokeWidth={3} />}
+      />
+    }
+  />
+);
 
 export const NoAnimation = () => (
-    <AreaChart
-      width={350}
-      height={250}
-      data={singleDateData}
-      series={<AreaSeries animated={false} />}
-    />
-  );
+  <AreaChart
+    width={350}
+    height={250}
+    data={singleDateData}
+    series={<AreaSeries animated={false} />}
+  />
+);
 
 export const NonZero = () => (
-    <AreaChart
-      width={350}
-      height={250}
-      data={nonZeroDateData as ChartDataShape[]}
-    />
-  );
+  <AreaChart
+    width={350}
+    height={250}
+    data={nonZeroDateData as ChartDataShape[]}
+  />
+);
 
 NonZero.story = {
-  name: 'Non-Zero',
+  name: 'Non-Zero'
 };
 
 export const Interval = () => (
-    <AreaChart
-      width={350}
-      height={250}
-      data={singleDateData}
-      xAxis={
-        <LinearXAxis
-          type="time"
-          tickSeries={<LinearXAxisTickSeries interval={timeDay} />}
-        />
-      }
-    />
-  );
+  <AreaChart
+    width={350}
+    height={250}
+    data={singleDateData}
+    xAxis={
+      <LinearXAxis
+        type="time"
+        tickSeries={<LinearXAxisTickSeries interval={timeDay} />}
+      />
+    }
+  />
+);
 
 export const Autosize = () => (
-    <div style={{ width: '50vw', height: '50vh', border: 'solid 1px red' }}>
-      <AreaChart data={singleDateData} />
-    </div>
-  );
+  <div style={{ width: '50vw', height: '50vh', border: 'solid 1px red' }}>
+    <AreaChart data={singleDateData} />
+  </div>
+);
+
+export const SingleValue = () => (
+  <AreaChart data={[singleDateData[0]]} width={350} height={250} />
+);
 
 export const Performance = () =>
-    range(15).map((i) => (
-      <div
-        key={i}
-        style={{
-          width: '250px',
-          height: '250px',
-          border: 'solid 1px green',
-          margin: '25px',
-          display: 'inline-block'
-        }}
-      >
-        <AreaChart data={singleDateData} />
-      </div>
-    ));
+  range(15).map((i) => (
+    <div
+      key={i}
+      style={{
+        width: '250px',
+        height: '250px',
+        border: 'solid 1px green',
+        margin: '25px',
+        display: 'inline-block'
+      }}
+    >
+      <AreaChart data={singleDateData} />
+    </div>
+  ));
 
 export const BigInt = () => (
-    <AreaChart width={350} height={250} data={singleDateBigIntData} />
-  );
+  <AreaChart width={350} height={250} data={singleDateBigIntData} />
+);
