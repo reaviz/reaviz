62d387f694d3d140a7bf8aed22312a1018e3c9ea
Austin <amcdaniel2@gmail.com>
2022-05-09 09:59:50 -0400

    add comment

--- src/common/Motion/config.ts
+++ src/common/Motion/config.ts
@@ -2,6 +2,7 @@ export const DEFAULT_TRANSITION = {
   type: 'spring',
   velocity: 5,
   damping: 20,
+  // https://github.com/framer/motion/issues/1513#issuecomment-1121133717
   restDelta: 0.01,
   restSpeed: 0.01
 };
