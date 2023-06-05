3fd905a2d4a74d44b72d33db76214a0f9cedc198
amcdnl <amcdaniel2@gmail.com>
2023-06-05 09:45:20 -0400

    updates

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,8 +7,7 @@ const config: StorybookConfig = {
   ],
   addons: [
     '@storybook/addon-storysource',
-    '@storybook/addon-essentials',
-
+    '@storybook/addon-essentials'
   ],
   framework: {
     name: '@storybook/react-vite',
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,13 +1,20 @@
 import theme from './theme';
+import { Preview } from '@storybook/react';
 
-export const parameters = {
-  layout: 'centered',
-  docs: {
-    theme
-  },
-  options: {
-    storySort: {
-      order: ['Docs', ['Intro', 'Getting Started', 'Chart Types', 'Utils', 'Advanced']]
+const preview: Preview = {
+  parameters: {
+    layout: 'centered',
+    controls: { hideNoControlsWarning: true },
+    actions: { argTypesRegex: '^on.*' },
+    docs: {
+      theme
+    },
+    options: {
+      storySort: {
+        order: ['Docs', ['Intro', 'Getting Started', 'Chart Types', 'Utils', 'Advanced']]
+      },
     }
   }
 };
+
+export default preview;
