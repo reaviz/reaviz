3ec14deb28d71b15a0a3c72e14b4e6574d486930
amcdnl <amcdaniel2@gmail.com>
2023-06-14 14:59:49 -0400

    rewrite calculateSize for webpack :()

--- src/common/utils/size.ts
+++ src/common/utils/size.ts
@@ -1,21 +1,57 @@
-import calculateSize from 'calculate-size';
+export interface Dimensions {
+  height: number;
+  width: number;
+}
+
+const cache: { [key: string]: Dimensions } = {};
 
 export const calculateDimensions = (
   text: string,
   fontFamily: string,
   fontSize: string | number
-) => {
-  // SSR Rendering doesn't support canvas measurements
-  // we have to make a guess in this case...
-  if (typeof document === 'undefined') {
-    return {
-      width: text.length * 8,
-      height: 25
-    };
+): Dimensions => {
+  const key = `${text}_${fontFamily}_${fontSize}`;
+
+  // Check if we have a cache hit
+  if (cache[key]) {
+    return cache[key];
   }
 
-  return calculateSize(text, {
-    font: fontFamily,
-    fontSize: `${fontSize}px`
-  });
+  // If we are in a Node.js environment
+  if (typeof window === 'undefined' || typeof document === 'undefined') {
+    const dimensions = {
+      height: text.length,
+      width: text.length
+    };
+
+    cache[key] = dimensions;
+
+    return dimensions;
+  }
+
+  // Create a canvas element and get the context
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+
+  if (!context) {
+    throw new Error('Failed to get canvas context');
+  }
+
+  // Set the font settings
+  context.font = `${fontSize} ${fontFamily}`;
+
+  // Measure the text
+  const metrics = context.measureText(text);
+
+  // Calculate height using the font size
+  const height = parseInt(typeof fontSize === 'string' ? fontSize : fontSize.toString(), 10);
+  const dimensions = {
+    height,
+    width: metrics.width
+  };
+
+  // Store the result in the cache for future calls
+  cache[key] = dimensions;
+
+  return dimensions;
 };
