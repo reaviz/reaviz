50a39b4cd84cec722420380b23b334c55f938e74
Evgeniy <evgeniy.kulinich@mev.com>
2023-09-08 15:14:04 +0300

    Convert LinearAxisTickSeries to FC

--- src/common/Axis/LinearAxis/LinearAxisTickSeries.tsx
+++ src/common/Axis/LinearAxis/LinearAxisTickSeries.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, ReactElement } from 'react';
+import React, { FC, Fragment, ReactElement, useCallback, useMemo } from 'react';
 import {
   LinearAxisTickLabel,
   LinearAxisTickLabelProps
@@ -42,19 +42,23 @@ interface ProcessedTick {
   half: 'start' | 'end' | 'center';
 }
 
-export class LinearAxisTickSeries extends Component<LinearAxisTickSeriesProps> {
-  static defaultProps: Partial<LinearAxisTickSeriesProps> = {
-    line: <LinearAxisTickLine />,
-    label: <LinearAxisTickLabel />,
-    tickSize: 30
-  };
+export const LinearAxisTickSeries: FC<Partial<LinearAxisTickSeriesProps>> = ({
+  scale,
+  orientation,
+  height,
+  width,
+  label,
+  tickSize,
+  tickValues,
+  interval,
+  line,
+  axis,
+}) => {
 
   /**
    * Gets the adjusted scale given offsets.
    */
-  getAdjustedScale() {
-    const { scale } = this.props;
-
+  const getAdjustedScale = useCallback(() => {
     if (scale.bandwidth) {
       let offset = scale.bandwidth() / 2;
       if (scale.round()) {
@@ -65,67 +69,30 @@ export class LinearAxisTickSeries extends Component<LinearAxisTickSeriesProps> {
     } else {
       return (d) => +scale(d);
     }
-  }
+  }, [scale]);
 
   /**
    * Gets the x/y position for a given tick.
    */
-  getPosition(scaledTick: number) {
-    const { orientation } = this.props;
-
+  const getPosition = useCallback((scaledTick: number) => {
     if (orientation === 'horizontal') {
       return { x: scaledTick, y: 0 };
     } else {
       return { x: 0, y: scaledTick };
     }
-  }
+  }, [orientation]);
 
   /**
    * Gets the dimension (height/width) this axis is calculating on.
    */
-  getDimension() {
-    const { height, width, orientation } = this.props;
+  const getDimension = useCallback(() => {
     return orientation === 'vertical' ? height : width;
-  }
-
-  /**
-   * Calculates the rotation angle that the ticks need to be shifted to.
-   * This equation will measure the length of the text in a external canvas
-   * object and determine what the longest label is and rotate until they fit.
-   */
-  getRotationAngle(ticks: any[]): number {
-    if (!this.props.label) {
-      return 0;
-    }
-
-    const label = this.props.label.props;
-    const dimension = this.getDimension();
-    const maxTicksLength = max(ticks, (tick) => tick.width);
-    let angle = 0;
-
-    if (label.rotation) {
-      if (label.rotation === true) {
-        let baseWidth = maxTicksLength;
-        const maxBaseWidth = Math.floor(dimension / ticks.length);
-
-        while (baseWidth > maxBaseWidth && angle > -90) {
-          angle -= 30;
-          baseWidth = Math.cos(angle * (Math.PI / 180)) * maxTicksLength;
-        }
-      } else {
-        angle = label.rotation;
-      }
-    }
-
-    return angle;
-  }
+  }, [height, orientation, width]);
 
   /**
    * Gets the formatted label of the tick.
    */
-  getLabelFormat(): (label: string) => string {
-    const { label, scale } = this.props;
-
+  const labelFormatFn = useMemo((): any =>  {
     if (label && label.props.format) {
       return label.props.format;
     } else if (scale.tickFormat) {
@@ -133,25 +100,24 @@ export class LinearAxisTickSeries extends Component<LinearAxisTickSeriesProps> {
     } else {
       return (v) => formatValue(v);
     }
-  }
+  }, [label, scale]);
 
   /**
    * Gets the ticks given the dimensions and scales and returns
    * the text and position.
    */
-  getTicks(): ProcessedTick[] {
-    const { scale, tickSize, tickValues, interval, axis, label } = this.props;
-    const dimension = this.getDimension();
+  const ticks = useMemo((): ProcessedTick[] => {
+    const dimension = getDimension();
     const maxTicks = getMaxTicks(tickSize, dimension);
     const ticks = getTicks(scale, tickValues, axis.type, maxTicks, interval);
-    const adjustedScale = this.getAdjustedScale();
-    const format = this.getLabelFormat();
+    const adjustedScale = getAdjustedScale();
+    const format = labelFormatFn;
     const midpoint = dimension / 2;
 
     return ticks.map((tick) => {
       const fullText = format(tick);
       const scaledTick = adjustedScale(tick);
-      const position = this.getPosition(scaledTick);
+      const position = getPosition(scaledTick);
       const text = ellipsize(fullText, 18);
       const size = label
         ? calculateDimensions(
@@ -174,39 +140,73 @@ export class LinearAxisTickSeries extends Component<LinearAxisTickSeriesProps> {
               : 'end'
       };
     });
-  }
+  }, [axis.type, getAdjustedScale, getDimension, getPosition, interval, label, labelFormatFn, scale, tickSize, tickValues]);
 
-  render() {
-    const { label, line, height, width, orientation } = this.props;
-    const ticks = this.getTicks();
-    const angle = this.getRotationAngle(ticks);
 
-    return (
-      <Fragment>
-        {ticks.map((tick, i) => (
-          <g key={i} transform={`translate(${tick.x}, ${tick.y})`}>
-            {line && (
-              <CloneElement<LinearAxisTickLineProps>
-                element={line}
-                height={height}
-                width={width}
-                orientation={orientation}
-              />
-            )}
-            {label && (
-              <CloneElement<LinearAxisTickLabelProps>
-                element={label}
-                text={tick.text}
-                fullText={tick.fullText}
-                half={tick.half}
-                angle={angle}
-                orientation={orientation}
-                line={line!}
-              />
-            )}
-          </g>
-        ))}
-      </Fragment>
-    );
-  }
-}
+  /**
+   * Calculates the rotation angle that the ticks need to be shifted to.
+   * This equation will measure the length of the text in a external canvas
+   * object and determine what the longest label is and rotate until they fit.
+   */
+  const angle = useMemo((): number => {
+    if (!label) {
+      return 0;
+    }
+
+    const labelProps = label.props;
+    const dimension = getDimension();
+    const maxTicksLength = max(ticks, (tick) => tick.width);
+    let angle = 0;
+
+    if (labelProps.rotation) {
+      if (labelProps.rotation === true) {
+        let baseWidth = maxTicksLength;
+        const maxBaseWidth = Math.floor(dimension / ticks.length);
+
+        while (baseWidth > maxBaseWidth && angle > -90) {
+          angle -= 30;
+          baseWidth = Math.cos(angle * (Math.PI / 180)) * maxTicksLength;
+        }
+      } else {
+        angle = labelProps.rotation;
+      }
+    }
+
+    return angle;
+  }, [getDimension, label, ticks]);
+
+  return (
+    <Fragment>
+      {ticks.map((tick, i) => (
+        <g key={i} transform={`translate(${tick.x}, ${tick.y})`}>
+          {line && (
+            <CloneElement<LinearAxisTickLineProps>
+              element={line}
+              height={height}
+              width={width}
+              orientation={orientation}
+            />
+          )}
+          {label && (
+            <CloneElement<LinearAxisTickLabelProps>
+              element={label}
+              text={tick.text}
+              fullText={tick.fullText}
+              half={tick.half}
+              angle={angle}
+              orientation={orientation}
+              line={line!}
+            />
+          )}
+        </g>
+      ))}
+    </Fragment>
+  );
+};
+
+LinearAxisTickSeries.defaultProps = {
+  line: <LinearAxisTickLine />,
+  label: <LinearAxisTickLabel />,
+  tickSize: 30
+};
+
