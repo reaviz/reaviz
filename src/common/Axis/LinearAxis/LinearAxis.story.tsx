0cc7da9133d66204289cc2a4fb29dbad361c5b09
Caleb Diehl <cdiehl@ap.org>
2023-09-14 10:13:41 -0700

    add print width to prettierrc file

--- src/common/Axis/LinearAxis/LinearAxis.story.tsx
+++ src/common/Axis/LinearAxis/LinearAxis.story.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { largeSignalChartData, categoryData } from '../../../../demo';
 import { ScatterPlot } from '../../../ScatterPlot';
 import { BarChart } from '../../../BarChart';
-import { LinearYAxis, LinearYAxisTickSeries, LinearYAxisTickLine, LinearYAxisTickLabel } from './LinearYAxis';
-import { LinearXAxis, LinearXAxisTickSeries, LinearXAxisTickLine, LinearXAxisTickLabel } from './LinearXAxis';
+import {
+  LinearYAxis,
+  LinearYAxisTickSeries,
+  LinearYAxisTickLine,
+  LinearYAxisTickLabel
+} from './LinearYAxis';
+import {
+  LinearXAxis,
+  LinearXAxisTickSeries,
+  LinearXAxisTickLine,
+  LinearXAxisTickLabel
+} from './LinearXAxis';
 
 export default {
   title: 'Utils/Axis/Linear'
@@ -11,7 +21,55 @@ export default {
 
 export const CenterAxes = () => {
   const data = [...largeSignalChartData].splice(0, 50);
-  return <ScatterPlot height={400} width={400} data={data} gridlines={null} yAxis={<LinearYAxis type="value" position="center" tickSeries={<LinearYAxisTickSeries line={<LinearYAxisTickLine position="center" />} label={<LinearYAxisTickLabel padding={3} />} />} />} xAxis={<LinearXAxis type="category" position="center" tickSeries={<LinearXAxisTickSeries line={<LinearXAxisTickLine position="center" />} label={<LinearXAxisTickLabel padding={3} />} />} />} />;
+  return (
+    <ScatterPlot
+      height={400}
+      width={400}
+      data={data}
+      gridlines={null}
+      yAxis={
+        <LinearYAxis
+          type="value"
+          position="center"
+          tickSeries={
+            <LinearYAxisTickSeries
+              line={<LinearYAxisTickLine position="center" />}
+              label={<LinearYAxisTickLabel padding={3} />}
+            />
+          }
+        />
+      }
+      xAxis={
+        <LinearXAxis
+          type="category"
+          position="center"
+          tickSeries={
+            <LinearXAxisTickSeries
+              line={<LinearXAxisTickLine position="center" />}
+              label={<LinearXAxisTickLabel padding={3} />}
+            />
+          }
+        />
+      }
+    />
+  );
 };
 
-export const CustomLabelRotation = () => <BarChart width={350} height={350} data={categoryData} xAxis={<LinearXAxis position="end" type="category" tickSeries={<LinearXAxisTickSeries label={<LinearXAxisTickLabel rotation={-90} />} />} />} />;
+export const CustomLabelRotation = () => (
+  <BarChart
+    width={350}
+    height={350}
+    data={categoryData}
+    xAxis={
+      <LinearXAxis
+        position="end"
+        type="category"
+        tickSeries={
+          <LinearXAxisTickSeries
+            label={<LinearXAxisTickLabel rotation={-90} />}
+          />
+        }
+      />
+    }
+  />
+);
