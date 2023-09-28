501bb74b35c69c84ab9e7b176b7799758bc775da
AdminAkai <akatoirotech@gmail.com>
2023-09-28 06:46:46 -0400

    cleaned up Marker comments and unused functions

--- src/common/Marker/Marker.tsx
+++ src/common/Marker/Marker.tsx
@@ -2,56 +2,9 @@ import React, { FC, ReactElement, useCallback } from 'react';
 import { MarkerLabel, MarkerLabelProps } from '../MarkerLabel';
 import { CloneElement } from 'rdk';
 import { ChartInternalDataShape } from '../data';
-import { Line, LineProps } from '../../AreaChart';
+import { LineProps } from '../../AreaChart';
 
-// export interface MarkerProps extends LineProps {
-//   /**
-//    * Placement of the marker on the graph with respect to the dataset.
-//    */
-//   value: number;
-
-//   /**
-//    * Color of the marker.
-//    */
-//   color: string;
-
-//   /**
-//    * Size of the marker.
-//    */
-//   strokeWidth: number;
-
-//   /**
-//    * Width of the chart. Set internally by Graph.
-//    */
-//   width: number;
-
-//   /**
-//    * Width of the chart. Set internally by Graph.
-//    */
-//   height?: number;
-
-//   /**
-//    * D3 scale for Y Axis. Set internally by Graph.
-//    */
-//   yScale: number;
-
-//   /**
-//    * Parsed data shape. Set internally by Graph.
-//    */
-//   data: ChartInternalDataShape[];
-
-//   /**
-//    * Marker Label for the Marker.
-//    */
-//   label?: ReactElement<MarkerLabelProps, typeof MarkerLabel> | string | null;
-
-//   /**
-//    * Changing direction of the marker to match horizontal graphs. Set internally by Graph.
-//    */
-//   horizontal?: boolean;
-// }
-
-export interface MarkerProps extends LineProps {
+export interface MarkerProps {
   /**
    * Placement of the marker on the graph with respect to the dataset.
    */
@@ -130,7 +83,7 @@ export const Marker: FC<Partial<MarkerProps>> = ({
         )}
       </>
     ),
-    [label, width, value, horizontal]
+    [label, width, value, horizontal, xScale, yScale]
   );
 
   const directionProps = {
@@ -140,23 +93,6 @@ export const Marker: FC<Partial<MarkerProps>> = ({
     y2: horizontal ? height : yScale(value)
   };
 
-  const getColor = () => color;
-
-  // return (
-  //   <svg>
-  //     <g dominantBaseline="central">
-  //       <Line
-  //         data={data}
-  //         xScale={xScale}
-  //         yScale={yScale}
-  //         color={getColor}
-  //         lineValue={value}
-  //         lineMarker
-  //       />
-  //       {renderMarkerLabel()}
-  //     </g>
-  //   </svg>
-  // );
   return (
     <svg>
       <g dominantBaseline="central">
