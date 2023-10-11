2e79074c2b13c92d9069e8801c413077da0943d8
Grant Steffen <gsteffen1203@gmail.com>
2023-10-11 11:44:18 -0700

    Remove unneccessary logic in ChartContainer

--- src/common/containers/ChartContainer.tsx
+++ src/common/containers/ChartContainer.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useState, useMemo, useEffect } from 'react';
+import React, { FC, useCallback, useState, useMemo } from 'react';
 import { Margins, getDimension } from '../utils/dimensions';
 import useDimensions from 'react-cool-dimensions';
 import { useId } from 'rdk';
@@ -95,7 +95,6 @@ export const ChartContainer: FC<ChartContainerProps> = ({
   xAxisVisible,
   yAxisVisible,
   id,
-  onChartDimensionsChanged,
   ...rest
 }) => {
   const curId = useId(id);
@@ -180,12 +179,6 @@ export const ChartContainer: FC<ChartContainerProps> = ({
     rest.height !== undefined && rest.height !== null ? rest.height : '100%';
   const styleWidth =
     rest.width !== undefined && rest.width !== null ? rest.width : '100%';
-
-  useEffect(() => {
-    if (onChartDimensionsChanged) {
-      onChartDimensionsChanged({ chartHeight: childProps.chartHeight, chartWidth: childProps.chartWidth });
-    }
-  }, [childProps.chartHeight, childProps.chartWidth, onChartDimensionsChanged]);
 
   return (
     <div
