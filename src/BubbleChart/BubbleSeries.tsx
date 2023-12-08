9651b7870d996663095449d30e71850ac9730b50
Grant Steffen <gsteffen1203@gmail.com>
2023-12-08 14:57:55 -0800

    Use safe-identifier on bubble id's

--- src/BubbleChart/BubbleSeries.tsx
+++ src/BubbleChart/BubbleSeries.tsx
@@ -8,6 +8,7 @@ import { CloneElement } from 'rdk';
 import invert from 'invert-color';
 import chroma from 'chroma-js';
 import { DEFAULT_TRANSITION } from '../common/Motion';
+import { identifier } from 'safe-identifier';
 
 export interface BubbleSeriesProps {
   /**
@@ -78,14 +79,14 @@ export const BubbleSeries: FC<Partial<BubbleSeriesProps>> = ({
       >
         <CloneElement<BubbleProps>
           element={bubble}
-          id={`${id}-${item.data.key}-bubble`}
+          id={identifier(`${id}-${item.data.key}-bubble`)}
           animated={animated}
           data={item}
           fill={fill}
         />
         <CloneElement<BubbleLabelProps>
           element={label}
-          id={`${id}-${item.data.key}-label`}
+          id={identifier(`${id}-${item.data.key}-label`)}
           animated={animated}
           data={item}
           fill={textFill}
