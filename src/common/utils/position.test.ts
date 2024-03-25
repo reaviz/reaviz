0a707214eb77679c0d22b3837fe8e18d382c9889
Grant Steffen <gsteffen1203@gmail.com>
2024-03-25 13:55:00 -0700

    Update unit tests

--- src/common/utils/position.test.ts
+++ src/common/utils/position.test.ts
@@ -6,14 +6,12 @@ import {
 import { describe, test } from 'vitest';
 
 describe('getClosestContinousScalePoint', () => {
+  const data = [{ x: 4 }, { x: 6 }];
+  const scale = scaleLinear().rangeRound([0, 300]).domain([4, 6]);
+
   test('returns the closest point for an invertible/continous scale', ({
     expect
   }) => {
-    const data = [{ x: 4 }, { x: 6 }];
-
-    let scale = scaleLinear().rangeRound([0, 300]);
-    scale = scale.domain([4, 6]);
-
     const result = getClosestContinousScalePoint(150, scale, data);
 
     expect(result).toEqual({ x: 6 });
@@ -22,11 +20,6 @@ describe('getClosestContinousScalePoint', () => {
   test('returns the closest point rounded down for an invertible/continous scale', ({
     expect
   }) => {
-    const data = [{ x: 4 }, { x: 6 }];
-
-    let scale = scaleLinear().rangeRound([0, 300]);
-    scale = scale.domain([4, 6]);
-
     const result = getClosestContinousScalePoint(150, scale, data, {
       roundDown: true
     });
@@ -36,23 +29,18 @@ describe('getClosestContinousScalePoint', () => {
 });
 
 describe('getClosestBandScalePoint', () => {
+  const data = [{ x: 'a' }, { x: 'b' }, { x: 'c' }];
+  const scale = scaleBand().rangeRound([0, 100]).domain(['a', 'b', 'c']);
+
   test('returns the closest point for a band scale rounded down', ({
     expect
   }) => {
-    const scale = scaleBand().rangeRound([0, 100]).domain(['a', 'b', 'c']);
-
-    const data = [{ x: 'a' }, { x: 'b' }, { x: 'c' }];
-
     const result = getClosestBandScalePoint(32, scale, data);
 
     expect(result).toEqual({ x: 'a' });
   });
 
   test('returns the closest point for a band scale', ({ expect }) => {
-    const scale = scaleBand().rangeRound([0, 100]).domain(['a', 'b', 'c']);
-
-    const data = [{ x: 'a' }, { x: 'b' }, { x: 'c' }];
-
     const result = getClosestBandScalePoint(32, scale, data, {
       roundClosest: true
     });
