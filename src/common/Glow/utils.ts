4e86f08a286672e8df98295686bf21f0cedbbd6d
Grant Steffen <gsteffen1203@gmail.com>
2024-04-02 09:50:24 -0700

    Use chroma js

--- src/common/Glow/utils.ts
+++ src/common/Glow/utils.ts
@@ -1,33 +1,10 @@
 import { Glow } from './Glow';
+import chroma from 'chroma-js';
 
 export interface generateGlowStylesInput {
   glow?: Glow;
   colorSchemeColor?: string;
 }
-
-/**
- * Convert a color to RGBA format.
- * @param color - The color to convert in HEX, RGB, or RGBA format.
- * @param opacity - The opacity to apply to the color.
- * @returns The color in RGBA format.
- */
-export const convertColorToRGBA = (color: string, opacity: number) => {
-  if (color.startsWith('#')) {
-    let r = parseInt(color.slice(1, 3), 16),
-      g = parseInt(color.slice(3, 5), 16),
-      b = parseInt(color.slice(5, 7), 16);
-
-    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
-  } else if (color.startsWith('rgba')) {
-    let rgbaComponents = color.split(',');
-    rgbaComponents[3] = ` ${opacity})`;
-    return rgbaComponents.join(',');
-  } else if (color.startsWith('rgb')) {
-    return color.replace(')', `, ${opacity})`).replace('rgb', 'rgba');
-  }
-
-  return color;
-};
 
 export const generateGlowStyles = ({
   glow,
@@ -43,7 +20,7 @@ export const generateGlowStyles = ({
     opacity = 1
   } = glow;
 
-  color = convertColorToRGBA(color, opacity);
+  color = chroma(color).alpha(opacity).css();
 
   return blur
     ? { filter: `drop-shadow(${x}px ${y}px ${blur}px ${color})` }
