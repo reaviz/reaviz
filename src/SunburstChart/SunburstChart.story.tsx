e7822c76cf57b49e8fe9990ddc8f88175fada075
amcdnl <amcdaniel2@gmail.com>
2024-05-30 08:54:48 -0400

    fix import

--- src/SunburstChart/SunburstChart.story.tsx
+++ src/SunburstChart/SunburstChart.story.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { SunburstChart } from './SunburstChart';
-import { heatmapSimpleData } from '../../demo';
+import { heatmapSimpleData } from 'reaviz-data-utils';
 
 export default {
   title: 'Charts/Sunburst Chart',
   component: SunburstChart,
-  subcomponents: {
-  }
+  subcomponents: {}
 };
 
 export const Simple = () => (
-  <SunburstChart
-    height={450}
-    width={450}
-    data={heatmapSimpleData}
-  />
+  <SunburstChart height={450} width={450} data={heatmapSimpleData} />
 );
