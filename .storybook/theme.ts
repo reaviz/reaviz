3c987cf3704999014de639a329dabbb79ebefea9
amcdnl <amcdaniel2@gmail.com>
2024-06-04 13:38:42 -0400

    remove old docs, tweak sb theme

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -5,5 +5,16 @@ export default create({
   base: 'dark',
   brandTitle: 'REAVIZ',
   brandUrl: 'https://github.com/reaviz/reaviz',
-  brandImage: ReavizLogo
+  brandImage: ReavizLogo,
+  colorPrimary: '#0C77FF',
+  appContentBg: '#11111F',
+  appPreviewBg: '#11111F',
+  fontBase: 'Inter,Arial,Helvetica,sans-serif',
+  fontCode: 'ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace',
+  appBg: '#11111F',
+  textColor: '#FFFFFF',
+  barBg: '#11111F',
+  appBorderColor: '#1f2937',
+  inputBorder: '#1f2937',
+  buttonBorder: '#1f2937'
 });
--- docs/tools/templates/reaviz-codesandbox-template/src/App.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function App() {
-  return <h1>Hello reaviz!</h1>;
-}
