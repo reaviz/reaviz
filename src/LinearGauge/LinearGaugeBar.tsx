bf1dc33c857612bcf121f194a67235dd1f67e52b
SerhiiTsybulskyi <c4r0n0s@gmail.com>
2024-12-10 16:41:53 +0200

    LinearGauge migration

--- src/LinearGauge/LinearGaugeBar.tsx
+++ src/LinearGauge/LinearGaugeBar.tsx
@@ -9,17 +9,16 @@ import {
 export type LinearGaugeBarProps = BarProps;
 
 export const LinearGaugeBar: FC<Partial<LinearGaugeBarProps>> = (props) => (
-  <Bar {...props} />
+  <Bar
+    tooltip={
+      <ChartTooltip
+        {...CHART_TOOLTIP_DEFAULT_PROPS}
+        placement="top"
+        content={(data) => (
+          <TooltipTemplate value={{ y: data.value, x: data.y }} />
+        )}
+      />
+    }
+    {...props}
+  />
 );
-
-LinearGaugeBar.defaultProps = {
-  tooltip: (
-    <ChartTooltip
-      {...CHART_TOOLTIP_DEFAULT_PROPS}
-      placement="top"
-      content={(data) => (
-        <TooltipTemplate value={{ y: data.value, x: data.y }} />
-      )}
-    />
-  )
-};
