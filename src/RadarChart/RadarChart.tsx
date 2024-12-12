a39a070860164356433bf778a647e10480ad42f7
Evgeniy <evgeniy.kulinich@mev.com>
2024-12-12 10:25:08 +0200

    Remove default props from RadarChartSeries components

--- src/RadarChart/RadarChart.tsx
+++ src/RadarChart/RadarChart.tsx
@@ -6,18 +6,29 @@ import {
   RadialAxisArcLine,
   RadialAxisArcSeries
 } from '@/common/Axis/RadialAxis';
-import { RadarChartSeries } from './RadarChartSeries';
+import {
+  RADAR_CHART_SERIES_DEFAULT_PROPS,
+  RadarChartSeries
+} from './RadarChartSeries';
 
 export interface RadarChartProps extends RadialAreaChartProps {
   data: ChartNestedDataShape[];
 }
 
 export const RadarChart: FC<Partial<RadarChartProps>> = (props) => (
-  <RadialAreaChart {...RADAR_CHART_DEFAULT_PROPS} {...props} />
+  <RadialAreaChart
+    {...props}
+    series={
+      <RadarChartSeries
+        {...RADAR_CHART_SERIES_DEFAULT_PROPS}
+        {...props?.series?.props}
+      />
+    }
+    {...RADAR_CHART_DEFAULT_PROPS}
+  />
 );
 
 const RADAR_CHART_DEFAULT_PROPS = {
-  series: <RadarChartSeries />,
   axis: (
     <RadialAxis
       type="category"
--- src/RadarChart/RadarChartSeries.tsx
+++ src/RadarChart/RadarChartSeries.tsx
@@ -12,7 +12,7 @@ export const RadarChartSeries: FC<Partial<RadarChartSeriesProps>> = (props) => (
   <RadialAreaSeries {...RADAR_CHART_SERIES_DEFAULT_PROPS} {...props} />
 );
 
-const RADAR_CHART_SERIES_DEFAULT_PROPS = {
+export const RADAR_CHART_SERIES_DEFAULT_PROPS = {
   area: null,
   type: 'grouped' as RadialPointSeriesType,
   symbols: <RadialPointSeries show />
